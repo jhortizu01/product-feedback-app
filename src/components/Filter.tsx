fa699fa0667becd3acf3632ab2cb37c01da949dd
Janika Hortizuela <JanikaHortizuela@rightpoint.com>
2022-05-19 14:27:06 -0600

    Refactor Filter.tsx

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -23,6 +23,23 @@ export const Filter = (props: IProps) => {
     setShowData(filteredItems);
   };
 
+  const filterCategories = ['ui', 'ux', 'enhancement', 'bug', 'feature'];
+
+  let category = filterCategories.map((category) => {
+    return (
+      <>
+        <input
+          type="radio"
+          id={category}
+          name="filter"
+          value={category}
+          onClick={() => filter(category)}
+        ></input>
+        <label htmlFor={category}> {category}</label>
+      </>
+    );
+  });
+
   return (
     <section className="filter">
       <div>
@@ -34,46 +51,7 @@ export const Filter = (props: IProps) => {
           onClick={showAll}
         ></input>
         <label htmlFor="all">All</label>
-        <input
-          type="radio"
-          id="ui"
-          name="filter"
-          value="ui"
-          onClick={() => filter('ui')}
-        ></input>
-        <label htmlFor="ui">UI</label>
-        <input
-          type="radio"
-          id="ux"
-          name="filter"
-          value="ux"
-          onClick={() => filter('ux')}
-        ></input>
-        <label htmlFor="ux">UX</label>
-        <input
-          type="radio"
-          id="enhancement"
-          name="filter"
-          value="enhancement"
-          onClick={() => filter('enhancement')}
-        ></input>
-        <label htmlFor="enhancement">Enhancement</label>
-        <input
-          type="radio"
-          id="bug"
-          name="filter"
-          value="bug"
-          onClick={() => filter('bug')}
-        ></input>
-        <label htmlFor="bug">Bug</label>
-        <input
-          type="radio"
-          id="feature"
-          name="filter"
-          value="feature"
-          onClick={() => filter('feature')}
-        ></input>
-        <label htmlFor="feature">Feature</label>
+        {category}
       </div>
     </section>
   );
