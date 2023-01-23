718ee2497a100bde4c2b1beba87e2293ae96f5ba
Janika Hortizuela <JanikaHortizuela@rightpoint.com>
2023-01-23 10:52:54 -0700

    Implement msw

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './pages/App';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
--- src/pages/CreateFeedback.tsx
+++ src/pages/CreateFeedback.tsx
@@ -48,15 +48,31 @@ export const CreateFeedback = (props: IProps) => {
   });
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    productRequests.push({
-      id: productRequests.length + 1,
-      title: data.title,
-      category: data.category,
-      upvotes: 0,
-      status: 'suggestion',
-      description: data.feedback,
-      comments: [],
-    });
+    // productRequests.push({
+    //   id: productRequests.length + 1,
+    //   title: data.title,
+    //   category: data.category,
+    //   upvotes: 0,
+    //   status: 'suggestion',
+    //   description: data.feedback,
+    //   comments: [],
+    // });
+
+    fetch('/api/productRequests', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        id: productRequests.length + 1,
+        title: data.title,
+        category: data.category,
+        upvotes: 0,
+        status: 'suggestion',
+        description: data.feedback,
+        comments: [],
+      }),
+    })
+      .then((response) => response.json())
+      .catch((error) => console.log(error));
 
     navigate('/');
   };
