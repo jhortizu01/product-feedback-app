e14fca457a7818b427e0dec5c4eb5e5135d87969
Janika Hortizuela <JanikaHortizuela@rightpoint.com>
2023-02-07 11:05:54 -0700

    Add configure eslint for testing

--- src/filter/Filter.test.js
+++ src/filter/Filter.test.js
@@ -5,19 +5,19 @@ describe('Filter Block', () => {
   test('Filter has a button called All', () => {
     render(<Filter />);
     const allButton = screen.getByRole('button', { name: 'All' });
-    expect(allButton.textContent).toBe('All');
+    expect(allButton).toHaveTextContent('All');
   });
 
   test('Filter has a button called UX', () => {
     render(<Filter />);
     const uxButton = screen.getByRole('button', { name: 'UX' });
-    expect(uxButton.textContent).toBe('UX');
+    expect(uxButton).toHaveTextContent('UX');
   });
 
   test('Filter has a button called UI', () => {
     render(<Filter />);
     const uiButton = screen.getByRole('button', { name: 'UI' });
-    expect(uiButton.textContent).toBe('UI');
+    expect(uiButton).toHaveTextContent('UI');
   });
 
   test('Filter has a button called Enhancement', () => {
@@ -25,18 +25,18 @@ describe('Filter Block', () => {
     const enhancementButton = screen.getByRole('button', {
       name: 'Enhancement',
     });
-    expect(enhancementButton.textContent).toBe('Enhancement');
+    expect(enhancementButton).toHaveTextContent('Enhancement');
   });
 
   test('Filter has a button called Bug', () => {
     render(<Filter />);
     const bugButton = screen.getByRole('button', { name: 'Bug' });
-    expect(bugButton.textContent).toBe('Bug');
+    expect(bugButton).toHaveTextContent('Bug');
   });
 
   test('Filter has a button called Feature', () => {
     render(<Filter />);
     const featureButton = screen.getByRole('button', { name: 'Feature' });
-    expect(featureButton.textContent).toBe('Feature');
+    expect(featureButton).toHaveTextContent('Feature');
   });
 });
